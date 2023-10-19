15870ea9146433eb3c3a8e4b528de2d4865417b6
Rodrigo <rdg.hora1@gmail.com>
2023-10-19 10:31:29 -0300

    Finish

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,8 +1,8 @@
-import t from 'prop-types'; // Importa o módulo 'prop-types' para definir as propriedades esperadas
+import t from "prop-types"; // Importa o módulo 'prop-types' para definir as propriedades esperadas
 
-import React from 'react'; // Importa o módulo 'react'
+import React from "react"; // Importa o módulo 'react'
 
-import './style.css'; // Importa um arquivo de estilo chamado 'style.css'
+import "./style.css"; // Importa um arquivo de estilo chamado 'style.css'
 
 export default function Form({
   date,
@@ -12,80 +12,73 @@ export default function Form({
 }) {
   return (
     <form className="main-form">
-      {' '}
+      {" "}
       {/* Cria um formulário com a classe CSS 'main-form' */}
       <label htmlFor="main-date" className="main-date">
-        {' '}
+        {" "}
         {/* Cria um rótulo para a data */}
-        <span>Data: </span>
-        {' '}
-        {/* Adiciona um texto "Data:" ao rótulo */}
+        <span>Data: </span> {/* Adiciona um texto "Data:" ao rótulo */}
         <input
           type="date"
           id="main-date"
           value={date}
           onChange={(event) => setDate(event.target.value)}
-        />
-        {' '}
+        />{" "}
         {/* Cria uma entrada de data com a data fornecida e uma função onChange para atualizar a data */}
       </label>
-
       <label htmlFor="more-days">
-        {' '}
+        {" "}
         {/* Cria um rótulo para o número de dias */}
-        <span>+/- Dias: </span>
-        {' '}
-        {/* Adiciona um texto "+/- Dias:" ao rótulo */}
+        <span>+/- Dias: </span> {/* Adiciona um texto "+/- Dias:" ao rótulo */}
         <input
           type="number"
           id="more-days"
           className="number-input"
           value={daysMonthsYears.days}
-          onChange={(event) => setDaysMonthsYears(
-            { ...daysMonthsYears, days: Number(event.target.value) },
-          )}
-        />
-        {' '}
+          onChange={(event) =>
+            setDaysMonthsYears({
+              ...daysMonthsYears,
+              days: Number(event.target.value),
+            })
+          }
+        />{" "}
         {/* Cria uma entrada numérica para os dias com a quantidade de dias fornecida e uma função onChange para atualizar os dias */}
       </label>
-
       <label htmlFor="more-months">
-        {' '}
+        {" "}
         {/* Cria um rótulo para o número de meses */}
-        <span>+/- Meses: </span>
-        {' '}
+        <span>+/- Meses: </span>{" "}
         {/* Adiciona um texto "+/- Meses:" ao rótulo */}
         <input
           type="number"
           id="more-months"
           className="number-input"
           value={daysMonthsYears.months}
-          onChange={(event) => setDaysMonthsYears({
-            ...daysMonthsYears,
-            months: Number(event.target.value),
-          })}
-        />
-        {' '}
+          onChange={(event) =>
+            setDaysMonthsYears({
+              ...daysMonthsYears,
+              months: Number(event.target.value),
+            })
+          }
+        />{" "}
         {/* Cria uma entrada numérica para os meses com a quantidade de meses fornecida e uma função onChange para atualizar os meses */}
       </label>
-
       <label htmlFor="more-years">
-        {' '}
+        {" "}
         {/* Cria um rótulo para o número de anos */}
-        <span>+/- Anos: </span>
-        {' '}
-        {/* Adiciona um texto "+/- Anos:" ao rótulo */}
+        <span>+/- Anos: </span> {/* Adiciona um texto "+/- Anos:" ao rótulo */}
         <input
           type="number"
           id="more-years"
           className="number-input"
           value={daysMonthsYears.years}
-          onChange={(event) => setDaysMonthsYears({
-            ...daysMonthsYears,
-            years: Number(event.target.value),
-          })}
-        />
-        {' '}
+          onChange={(event) =>
+            setDaysMonthsYears({
+              ...daysMonthsYears,
+              years: Number(event.target.value),
+            })
+          }
+        />{" "}
         {/* Cria uma entrada numérica para os anos com a quantidade de anos fornecida e uma função onChange para atualizar os anos */}
       </label>
     </form>
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'; // Importa o módulo 'react' e a função 'useState' para gerenciar estados
-import showDate from '../../utils/showDate'; // Importa uma função chamada 'showDate' de um arquivo de utilitário
-import Form from '../Form'; // Importa o componente 'Form' de um arquivo local
-import './style.css'; // Importa um arquivo de estilo chamado 'style.css'
+import React, { useState } from "react"; // Importa o módulo 'react' e a função 'useState' para gerenciar estados
+import showDate from "../../utils/showDate"; // Importa uma função chamada 'showDate' de um arquivo de utilitário
+import Form from "../Form"; // Importa o componente 'Form' de um arquivo local
+import "./style.css"; // Importa um arquivo de estilo chamado 'style.css'
 
 export default function Main() {
-  const [date, setDate] = useState(''); // Inicializa um estado para 'date' usando o useState hook
+  const [date, setDate] = useState(""); // Inicializa um estado para 'date' usando o useState hook
 
   const [daysMonthsYears, setDaysMonthsYears] = useState({
     days: 0,
@@ -14,10 +14,8 @@ export default function Main() {
 
   return (
     <>
-      <h1 className="main-title">Datas</h1>
-      {' '}
+      <h1 className="main-title">Datas</h1>{" "}
       {/* Cria um título principal com a classe CSS 'main-title' */}
-
       <Form
         date={date}
         setDate={setDate}
@@ -25,7 +23,6 @@ export default function Main() {
         setDaysMonthsYears={setDaysMonthsYears}
       />
       {/* Renderiza o componente 'Form' com as propriedades 'date', 'setDate', 'daysMonthsYears' e 'setDaysMonthsYears' */}
-
       <p className="date">{showDate(date, daysMonthsYears)}</p>
       {/* Cria um parágrafo com a classe CSS 'date' e exibe o resultado da função 'showDate' com os valores 'date' e 'daysMonthsYears' */}
     </>
